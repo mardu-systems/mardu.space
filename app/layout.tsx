c99e15861a880eeaaab005d47c2e553c7815c129
Luca Schöneberg <luca-schoeneberg@outlook.com>
2025-08-16 12:22:07 +0200

    Refactor `layout`: remove unused fonts, simplify body class, and clean up imports.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type {Metadata} from "next";
-import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import React from "react";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
     title: "mardu.space",
@@ -25,7 +15,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="de">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body className="antialiased">
         {children}
         </body>
         </html>
