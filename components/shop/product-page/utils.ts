ee287e3220866a241b85d9588a7ad88c67e506a9
Luca Schöneberg <luca-schoeneberg@outlook.com>
2025-08-19 11:17:58 +0200

    remove old shop

--- components/shop/product-page/utils.ts
+++
@@ -1,9 +0,0 @@
-export function formatPriceCents(
-  cents: number,
-  locale: string = "de-DE",
-  currency: string = "EUR"
-) {
-  return new Intl.NumberFormat(locale, { style: "currency", currency }).format(
-    cents / 100
-  );
-}
