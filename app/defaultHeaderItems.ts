396f064cc5ad60e3e2d537d1de1151acc95da4a7
Luca Schöneberg <luca-schoeneberg@outlook.com>
2025-08-19 11:19:05 +0200

    fix header links

--- app/defaultHeaderItems.ts
+++ app/defaultHeaderItems.ts
@@ -2,7 +2,7 @@ import {NavEntry} from "@/types/header";
 
 export const defaultHeaderItems: NavEntry[] = [
     {type: "link", label: "Home", href: "/"},
-    {type: "link", label: "Konfigurator", href: "/konfigurator"},
+    {type: "link", label: "Konfigurator", href: "/configurator"},
     {type: "link", label: "Das System", href: "/system"},
     {type: "link", label: "Kontakt", href: "/kontakt"},
 ];
