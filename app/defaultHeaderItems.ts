ef9bd5f1323346b589c99c1ce295af2ae901804b
Luca Schöneberg <luca-schoeneberg@outlook.com>
2025-08-20 20:36:27 +0200

    remove System because its not finished

--- app/defaultHeaderItems.ts
+++ app/defaultHeaderItems.ts
@@ -3,6 +3,9 @@ import {NavEntry} from "@/types/header";
 export const defaultHeaderItems: NavEntry[] = [
     {type: "link", label: "Home", href: "/"},
     {type: "link", label: "Konfigurator", href: "/configurator"},
-    {type: "link", label: "Das System", href: "/system"},
     {type: "link", label: "Kontakt", href: "/kontakt"},
+];
+
+export const x: NavEntry[] = [
+    {type: "link", label: "Das System", href: "/system"},
 ];
