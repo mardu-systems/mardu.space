188542c4ce524eef41bb88015189546bd91d2cef
Luca Schöneberg <luca-schoeneberg@outlook.com>
2026-01-17 00:47:01 +0100

    test: add vitest setup and whitepaper api tests

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  // @ts-ignore
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+    setupFiles: ['./vitest.setup.ts']
+  },
+});
